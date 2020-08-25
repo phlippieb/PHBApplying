0f66ab60b996a5241d8b0cc946d0a9d60dfcf6c4
phlippieb <phlippie.bosman@gmail.com>
2020-08-25 17:33:34 +0200

    Add support for SwiftPM

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PHBApplying",
+    products: [
+        .library(
+            name: "PHBApplying",
+            targets: ["PHBApplying"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "PHBApplying",
+            dependencies: []),
+   ]
+)
